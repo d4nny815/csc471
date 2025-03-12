1f9ba227f5374ffaafb25ef0fa9828d0baf60a9b
danny <d4nny815@gmail.com>
2025-03-11 20:00:22 -0700

    triangles and camera pitch and yaw

--- ray_tracer/src/geometry.cpp
+++ ray_tracer/src/geometry.cpp
@@ -30,3 +30,66 @@ bool Sphere::hit(const Ray& r, Interval t, HitRecord& hr) const {
 
     return true; 
 }
+
+//*============================================================================
+//* Triangle
+//*============================================================================
+
+// https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/moller-trumbore-ray-triangle-intersection.html
+#define EPISLON (1e-8)
+bool Triangle::hit(const Ray& r, Interval t, HitRecord& hr) const {
+    vec3 edge1 = v[1] - v[0];
+    vec3 edge2 = v[2] - v[0];
+
+    vec3 P = cross(r.dir, edge2);
+    float det = dot(edge1, P);
+
+    // if determinant is close to zero, the ray is parallel to the triangle
+    if (fabs(det) < EPISLON) return false;
+    if (det < 0) return false; // ignore back-facing triangles 
+    
+    // compute beta
+    float inv_det = 1.0 / det;
+    vec3 T = r.origin - v[0];
+    float beta = inv_det * dot(P, T);
+    if (!BARYCENTRIC_INTERVAL.contains(beta)) return false;
+    
+    // compute alpha and gamma
+    vec3 Q = cross(T, edge1);
+    float gamma = inv_det * dot(Q, r.dir);
+    if (!BARYCENTRIC_INTERVAL.contains(gamma)) return false;
+    float alpha = 1 - beta - gamma;
+    if (!BARYCENTRIC_INTERVAL.contains(alpha)) return false;
+
+    // compute the intersection point
+    float t_hit = inv_det * dot(Q, edge2);
+    if (!t.contains(t_hit)) return false;
+
+    hr.t = t_hit;
+    hr.point = r.at(t_hit);
+    hr.mat = mat;
+    hr.normal = unit_vector(cross(edge1, edge2));
+
+    return true;
+}
+
+//*============================================================================
+//* Mesh
+//*============================================================================
+
+bool Mesh::hit(const Ray& r, Interval t, HitRecord& hr) const {
+    bool hit_anything = false;
+    HitRecord temp_hr;
+    float closest_t = t.max;
+
+    for (const auto& face : faces) {
+        if (face.hit(r, Interval(t.min, closest_t), temp_hr)) {
+            hit_anything = true;
+            closest_t = temp_hr.t;
+            hr = temp_hr;
+        }
+    }
+
+    return hit_anything;
+}
+
--- ray_tracer/src/geometry.h
+++ ray_tracer/src/geometry.h
@@ -1,21 +1,48 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
+#include <vector>
 #include "primatives.h"
 #include "material.h"
 #include "object.h"
 
 class Sphere : public Hittable {
-    public: 
-        point3 center;
-        float radius;
-        shared_ptr<Material> mat;
+public: 
+    point3 center;
+    float radius;
+    shared_ptr<Material> mat;
+
+    Sphere() : center(0, 0, 0), radius(1.0) {}
+    Sphere(const point3& center, double radius, shared_ptr<Material> mat) : 
+        center(center), radius(std::fmax(0,radius)), mat(mat) {}
+
+    bool hit(const Ray& r, Interval t, HitRecord& hr) const override;
+};
+
+class Triangle : public Hittable {
+public:
+    point3 v[3];
+    shared_ptr<Material> mat;
+
+    Triangle(shared_ptr<Material> mat) : 
+        v{point3(-1, 0, 0), point3(1, 0, 0), point3(0, 1, 0)}, mat(mat) {}
+    Triangle(point3 p0, point3 p1, point3 p2, shared_ptr<Material> mat) : 
+        v{p0, p1, p2}, mat(mat) {}
+
+    bool hit(const Ray& r, Interval t, HitRecord& hr) const override;
+};
+
+static Interval BARYCENTRIC_INTERVAL = Interval(0, 1);
+
+class Mesh : public Hittable {
+    public:
+        std::vector<Triangle> faces;
     
-        Sphere() : center(0, 0, 0), radius(1.0) {}
-        Sphere(const point3& center, double radius, shared_ptr<Material> mat) : 
-            center(center), radius(std::fmax(0,radius)), mat(mat) {}
+        Mesh() = default;
+        Mesh(const std::vector<Triangle>& triangles) : faces(triangles) {}
     
         bool hit(const Ray& r, Interval t, HitRecord& hr) const override;
     };
+
 
 #endif /* geometry.h */
