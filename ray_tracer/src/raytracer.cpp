b71ff9752bdd208788e67b196a6263a9b600fa7e
danny <d4nny815@gmail.com>
2025-03-16 01:00:36 -0700

    time to profile

--- ray_tracer/src/raytracer.cpp
+++ ray_tracer/src/raytracer.cpp
@@ -31,18 +31,18 @@ int main(int argc, char** argv) {
     size_t samples_per_ray = argc > 3 ? atoi(argv[3]) : DEF_SAMPLES_PER_RAY;
     size_t child_rays = argc > 4 ? atoi(argv[4]) : DEF_CHILD_RAYS;
     
-    float look_radius = 4.0f;  // Distance from the camera to the look-at point
-    float pitch = 60;           // rotation around X-axis
-    float yaw = 180;             // rotation around Y-axis
+    float look_radius = 3.0f;  // Distance from the camera to the look-at point
+    float pitch = -10;           // rotation around X-axis
+    float yaw = -50;             // rotation around Y-axis
 
     // Convert pitch & yaw to Cartesian coordinates
+    const point3 lookat = point3(0, 1, 0);
     point3 cam_pos = point3(
         look_radius * sin(degrees_to_radians(yaw)) * cos(degrees_to_radians(pitch)),  
-        look_radius * sin(degrees_to_radians(pitch)),             
+        look_radius * sin(degrees_to_radians(pitch)) + lookat.y(),             
         look_radius * cos(degrees_to_radians(yaw)) * cos(degrees_to_radians(pitch))  
     );
-
-    const point3 lookat = point3(0, 0, -.5);
+    cam_pos.print(stderr);
 
     // Compute correct up vector
     const vec3 world_up(0, 1, 0);
@@ -51,61 +51,46 @@ int main(int argc, char** argv) {
     const vec3 up = cross(forward, right); // Ensure up is perpendicular
     
     const float defocus_ang_deg = 5.0f;
-    const float focus_dist = 3;
+    const float focus_dist = 1;
 
     Camera cam(filename, aspect_ratio, image_width, samples_per_ray, child_rays,
         VFOV_DEG, defocus_ang_deg, focus_dist, cam_pos, lookat, up);
 
     HittableList world;
-    
-    // Define different colors for each face
-    auto base_mat1 = make_shared<Lambertian>(Color(0.3, 0.7, 0.7)); // Cyan
-    auto base_mat2 = make_shared<Lambertian>(Color(0.2, 0.5, 0.2)); // Green
-
-    auto front_mat = make_shared<Lambertian>(Color(0.8, 0.1, 0.0)); // Red
-    auto right_mat = make_shared<Lambertian>(Color(0.2, 0.1, 0.8)); // Blue
-    auto back_mat  = make_shared<Lambertian>(Color(0.8, 0.8, 0.0)); // Yellow
-    auto left_mat  = make_shared<Metal>(Color(0.7, 0.2, 0.5), .9); // Purple
-
-    float pyramid_scale = 1;
-    std::vector<Triangle> triangles = {
-        Triangle(point3(-pyramid_scale, 0, -pyramid_scale), 
-                point3(-pyramid_scale, 0, pyramid_scale), 
-                point3(pyramid_scale, 0, pyramid_scale), base_mat1),
-                
-        Triangle(point3(pyramid_scale, 0, pyramid_scale), 
-                point3(pyramid_scale, 0, -pyramid_scale), 
-                point3(-pyramid_scale, 0, -pyramid_scale), base_mat2),
-        
-        Triangle(point3(-pyramid_scale, 0, pyramid_scale), 
-                point3(pyramid_scale, 0, pyramid_scale), 
-                point3(0, 3 * pyramid_scale, 0), front_mat), // Front
-        
-        Triangle(point3(pyramid_scale, 0, pyramid_scale), 
-                point3(pyramid_scale, 0, -pyramid_scale), 
-                point3(0, 3 * pyramid_scale, 0), right_mat),  // Right
-        
-        Triangle(point3(pyramid_scale, 0, -pyramid_scale), 
-                point3(-pyramid_scale, 0, -pyramid_scale), 
-                point3(0, 3 * pyramid_scale, 0), back_mat),  // Back
-        
-        Triangle(point3(-pyramid_scale, 0, -pyramid_scale), 
-                point3(-pyramid_scale, 0, pyramid_scale), 
-                point3(0, 3 * pyramid_scale, 0), left_mat) // Left
-    };
-
-    std::shared_ptr<Mesh> pyramid = std::make_shared<Mesh>(triangles);
-    world.add(pyramid);
+    auto ground_material = make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
+    world.add(make_shared<Sphere>(point3(0,-1000,0), 1000, ground_material));
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = rand_float();
+            point3 center(a + 0.9*rand_float(), 0.2, b + 0.9*rand_float());
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<Material> sphere_material;
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = Color::random() * Color::random();
+                    sphere_material = make_shared<Lambertian>(albedo);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = Color::random(0.5, 1);
+                    auto fuzz = rand_float(0, 1);
+                    sphere_material = make_shared<Metal>(albedo, fuzz);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<Dielectric>(1.5);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
 
     auto material1 = make_shared<Dielectric>(1.5);
     world.add(make_shared<Sphere>(point3(-2.5, 1, 0), 1.0, material1));
     auto material2 = make_shared<Lambertian>(Color(0xea3f24));
     world.add(make_shared<Sphere>(point3(3, 1, 0), 1.0, material2));
     auto material3 = make_shared<Metal>(Color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<Sphere>(point3(0, 1, -2), 1.0, material3));
-
-    auto ground_material = make_shared<Lambertian>(Color(0.8, 0.8, 0.8));
-    world.add(make_shared<Sphere>(point3(0,-1000,0), 1000, ground_material));
+    world.add(make_shared<Sphere>(point3(0, 1, 0), 1.0, material3));
 
     auto start = system_clock::now();
     cam.render(world);
